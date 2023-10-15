275c39c585bf1a78284e427695743a1ea217da93
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2023-10-15 14:34:17 -0300

    Add connection to MongoDB running locally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExchangeModule } from './exchange/exchange.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ExchangeModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ExchangeModule,
+  ],
 })
 export class AppModule { }
